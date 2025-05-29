9565d4c399e46a4bc712d3391a915d642f0dd834
CONCENTRIXNarendarReddy <CONCENTRIX_Narendar.Reddy@maruti.co.in>
2025-05-29 12:04:49 +0530

    Updated AppAvailability.js with changes

--- www/AppAvailability.js
+++ www/AppAvailability.js
@@ -1,39 +1,41 @@
-const cordova = require('cordova');
+const cordova = require("cordova");
 
 /**
  * AppAvailability plugin for Cordova
- * 
+ *
  * @class
  */
 class AppAvailability {
-    /**
-     * Check the availability of an app by its package name.
-     *
-     * @param {String}   packageName - The package name of the app to check.
-     * @param {Function} onSuccess   - Callback function to be called on success.
-     * @param {Function} onFail      - Callback function to be called on failure.
-     */
-    check(packageName, onSuccess, onFail) {
-        if (typeof packageName !== "string") {
-            return onFail(new Error("packageName must be a string"));
-        }
-        
-        if (!packageName.trim()) {
-            return onFail(new Error("packageName cannot be empty"));
-        }
-
-        cordova.exec(
-            result => {
-                onSuccess(result === 1);
-            },
-            error => {
-                onFail(error);
-            },
-            "AppAvailability",
-            "checkAvailability",
-            [packageName]
-        );
+  /**
+   * Check the availability of an app by its package name.
+   *
+   * @param {String}   packageName - The package name of the app to check.
+   * @param {Function} onSuccess   - Callback function to be called on success.
+   * @param {Function} onFail      - Callback function to be called on failure.
+   */
+  check(packageName, onSuccess, onFail) {
+    if (typeof packageName !== "string") {
+      return onFail(new Error("packageName must be a string"));
     }
+
+    if (!packageName.trim()) {
+      return onFail(new Error("packageName cannot be empty"));
+    }
+
+    cordova.exec(
+      (result) => {
+        // onSuccess(result === 1);
+        const isAvailable = result === true || result === 1;
+        onSuccess(isAvailable);
+      },
+      (error) => {
+        onFail(error);
+      },
+      "AppAvailability",
+      "checkAvailability",
+      [packageName]
+    );
+  }
 }
 
 // Register the plugin
